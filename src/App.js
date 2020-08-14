5bf6b9963f28c466ace7aa6bfeefcd3411839258
makerzenen <69445583+makerzenen@users.noreply.github.com>
2020-08-14 15:18:35 -0600

    Simple website

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <section className="App">
+        <article className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+          <h1 className="App-header-title">dustan curtis</h1>
+          <h2 className="App-header-subtitle">of the internet and its territories</h2>
+        </article>
+      </section>
     );
   }
 }
